c36510a4b8b96b9470def0cb67d485a75e086f98
nik0mon <nikolai@staywanderful.com>
2019-10-17 14:09:36 -0400

    works with gson

--- src/main/java/com/laasie/App.java
+++ src/main/java/com/laasie/App.java
@@ -1,15 +1,15 @@
 package com.laasie;
 
 import java.io.File;
+import java.io.InputStream;
 
-import javax.xml.XMLConstants;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBElement;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
 import javax.xml.transform.stream.StreamSource;
-import javax.xml.validation.Schema;
-import javax.xml.validation.SchemaFactory;
+
+import com.google.gson.Gson;
 
 import org.xml.sax.SAXException;
 
@@ -28,12 +28,15 @@ public final class App {
     public static void main(String[] args) throws JAXBException, SAXException{
         JAXBContext context = JAXBContext.newInstance(OTAHotelResNotifRQ.class);        
         Unmarshaller unmarshaller = context.createUnmarshaller();
-        SchemaFactory schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
-        Schema schema = schemaFactory.newSchema(new File("/Users/nik0mon/workspace/staywanderful/java/new-alt-reservation/new-alt-reservation/src/main/resources/altres.wsdl"));
-        unmarshaller.setSchema(schema);
-        File request = new File("/Users/nik0mon/workspace/staywanderful/java/new-alt-reservation/new-alt-reservation/src/main/resources/request.xml");
+        // SchemaFactory schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+        // Schema schema = schemaFactory.newSchema(new File("/Users/nik0mon/workspace/staywanderful/java/new-alt-reservation/new-alt-reservation/src/main/resources/altres.wsdl"));
+        // // unmarshaller.setSchema(schema);
+        File request = new File("/Users/nikomon/workspace/staywanderful/java/wips/jaxb/src/main/resources/request.xml");
         JAXBElement<OTAHotelResNotifRQ> root = unmarshaller.unmarshal(new StreamSource(request), OTAHotelResNotifRQ.class);
         OTAHotelResNotifRQ test = root.getValue();
         System.out.println(test);
-    }
+        Gson gson = new Gson();
+        String val = gson.toJson(test);
+        System.out.println(val.toString());
+     }
 }
